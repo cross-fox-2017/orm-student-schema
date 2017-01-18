c147973576436846c9e31187204712955327b346
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-18 13:47:00 +0700

    getalldata & getfullname done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict"
+
+let db = require('./models')
+
+// db.Student.bulkCreate([
+//   {firstname:'mangku', lastname:'widodo', birthdate:'1996-05-29'},
+//   {firstname:'bulok', lastname:'widodo', birthdate:'1994-03-19'},
+//   {firstname:'widodo', lastname:'widodo', birthdate:'1995-02-11'}
+//
+// ]).then(function (Student) {
+//   console.log('student add');
+// })
+
+db.Student.getAllData(function (students) {
+  students.forEach(function (student) {
+    console.log(`\nID : ${student.id}`);
+    console.log(`Firstname: ${student.firstname}`);
+    console.log(`Lastname: ${student.lastname}`);
+    console.log(`Fullname: ${student.getFullName()}`);
+  })
+})
--- models/student.js
+++ models/student.js
@@ -2,11 +2,22 @@
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING
+    lastname: DataTypes.STRING,
+    birthdate: DataTypes.DATE
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      },
+      getAllData: function (cb) {
+        Student.findAll().then(function (data) {
+          cb(data)
+        })
+      }
+    },
+    instanceMethods:{
+      getFullName: function () {
+        return `${this.firstname} ${this.lastname}`
       }
     }
   });
