4ecad08d66dfb4c2e195edf7e48c103e520782da
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-18 10:35:53 +0700

    release 0 & 1 done

---
+++ models/student.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Student = sequelize.define('Student', {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Student;
+};
