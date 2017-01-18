f167cececcd98b0a3f169b93373d6b2e7b6a9dbe
Muhammad Iqbal & yoma sofwan <pair+qblol.yomaswn@hacktiv8.com>
2017-01-18 14:32:26 +0700

    release 2 done

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 "use strict"
 var db  = require("./models")
 
-db.Student.bulkCreate([
-  {first_name:"Eri", last_name:"Irawan", telepon:"085777332",birthdate:"1991-02-11"},
-  {first_name:"Muhamad", last_name:"Iqbal", telepon:"085777332",birthdate:"1991-03-12"},
-  {first_name:"Radit", last_name:"dika", telepon:"085777332",birthdate:"1991-05-13"},
-  {first_name:"Yoma", last_name:"Sofwan", telepon:"085777332",birthdate:"1991-06-14"}
-]).then(function(students){
-  console.log(students);
+// db.Student.bulkCreate([
+//   {first_name:"Eri", last_name:"Irawan", telepon:"085777332",birthdate:"1991-02-11"},
+//   {first_name:"Muhamad", last_name:"Iqbal", telepon:"085777332",birthdate:"1991-03-12"},
+//   {first_name:"Radit", last_name:"dika", telepon:"085777332",birthdate:"1991-05-13"},
+//   {first_name:"Yoma", last_name:"Sofwan", telepon:"085777332",birthdate:"1991-06-14"}
+// ]).then(function(students){
+//   console.log(students);
+// })
+
+db.Student.findById(1).then(function(student){
+  console.log(student.getFullName());
+});
+
+db.Student.findById(1).then(function(student){
+  console.log(student.getAge());
+});
+
+db.Student.getAllData(function(students){
+  students.forEach(function(student){
+    console.log("ID : " + student.id);
+    console.log("Firs Name : " + student.first_name);
+    console.log("Last Name : " + student.last_name);
+    console.log("Telepon   : " + student.telepon);
+    console.log("Full Name : " + student.getFullName)
+  })
 })
--- models/student.js
+++ models/student.js
@@ -9,12 +9,30 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+
+      },
+      getAllData : function(callBack){
+        Student.findAll().then(function(data){
+          callBack(data)
+        })
+      }
+    },
+
+    instanceMethods: {
+      getFullName: function() {
+        return (this.first_name + " " + this.last_name)
+      },
+      getAge: function(){
+        let today = new Date();
+        let age = today.getFullYear() - this.birthdate.getFullYear();
+        let m = today.getMonth() - this.birthdate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < this.birthdate.getDate())) {
+          age--;
+        }
+        return age;
       }
     }
-    // 
-    // instanceMethod:{
-    //
-    // }
+
   });
   return Student;
 };
