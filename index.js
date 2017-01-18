65ef2e2da4f85a8fcc49301596d311d88db21235
Syanmil <syanmil@yahoo.com>
2017-01-18 14:11:34 +0700

    release 4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict"
+
+const db = require('./models')
+const faker = require('faker')
+
+// db.Student.bulkCreate(
+//   [{firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05')},
+// {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05')},
+// {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05')},
+// {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05')}
+// ]
+// )
+console.log(db.Student);
+
+db.Student.findById(2).then(function(data){
+  console.log(data.getFullName());
+})
+db.Student.findById(2).then(function(data){
+  console.log(data.getAge());
+})
+db.Student.getAllData()
---
+++ models/student.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Student = sequelize.define('Student', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    email: {
+      type: DataTypes.STRING,
+      validate: {isEmail: true},
+      unique: true
+    },
+    phone: {
+      type: DataTypes.STRING,
+      validate: {len: [10,13], isAlpha; false, isAlphanumeric: false}
+    }
+    height: {
+      type: DataTypes.INTEGER,
+      validate: {min: 150}
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      },
+      getAllData: function(){
+        Student.findAll().then(function(data){
+          data.forEach(function(data){
+            console.log(`id: ${data.dataValues.id}\nFullname: "${data.getFullName()}"\nage: ${data.getAge()}\n`);
+          })
+        })
+      }
+    },
+    instanceMethods: {
+      getFullName: function(){
+        return (`${this.firstName} ${this.lastName}`)
+      },
+      getAge: function(){
+        var birthday = new Date(this.birthday);
+        return Math.floor((Date.now() - birthday) / (31557600000));
+      }
+    }
+  });
+  return Student;
+};
