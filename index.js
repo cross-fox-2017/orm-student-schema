b1cc4b560c7528122b74d5661f8c251f474d9f24
Muhammad Iqbal & yoma sofwan <pair+qblol.yomaswn@hacktiv8.com>
2017-01-18 12:17:19 +0700

    create data dummy

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict"
+var db  = require("./models")
+
+db.Student.bulkCreate([
+  {first_name:"Eri", last_name:"Irawan", telepon:"085777332",birthdate:"1991-02-11"},
+  {first_name:"Muhamad", last_name:"Iqbal", telepon:"085777332",birthdate:"1991-03-12"},
+  {first_name:"Radit", last_name:"dika", telepon:"085777332",birthdate:"1991-05-13"},
+  {first_name:"Yoma", last_name:"Sofwan", telepon:"085777332",birthdate:"1991-06-14"}
+]).then(function(students){
+  console.log(students);
+})
--- models/student.js
+++ models/student.js
@@ -3,13 +3,18 @@ module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    telepon: DataTypes.INTEGER
+    telepon: DataTypes.INTEGER,
+    birthdate: DataTypes.DATE
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
     }
+    // 
+    // instanceMethod:{
+    //
+    // }
   });
   return Student;
 };
